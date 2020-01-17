e3e8af8ccb44053b01ff53797161337ec8654d6c
Niklas <wulms@uni-muenster.de>
2020-01-17 20:30:36 +0100

    update repository - script will be edited soon

---
+++ script/BIDS-Direct-ConverteR.R
@@ -0,0 +1,34 @@
+# BIDS-Direct-ConverteR
+
+
+
+## load libraries
+
+
+
+## create folders
+
+
+
+## dicom2niix
+
+
+
+## json indexing
+
+
+
+## json wrangling
+
+
+
+## Dashboard
+
+
+
+## nii_temp2BIDS
+
+
+## Export2Cooperate
+
+
