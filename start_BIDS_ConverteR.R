ede89326b264adb273f01d2db8b43fbd7da6525a
Niklas Wulms <wulms@uni-muenster.de>
2021-07-07 11:47:44 +0200

    # removed tests

--- start_BIDS_ConverteR.R
+++ start_BIDS_ConverteR.R
@@ -3,9 +3,6 @@ args = commandArgs(trailingOnly=TRUE)
 
 
 
-
-# print the number of all dicom folders
-print(paste("You have added ", length(dicomdirs), "DICOM directories to the converter."))
 # start the converter
 source(paste0(getwd(), "/script/BIDS-Direct-ConverteR.R"))
 
