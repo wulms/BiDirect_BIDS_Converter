22eff89eb9404ff528da8dfd3d8cabfd0a91fabc
Niklas Wulms <wulms@uni-muenster.de>
2021-07-07 11:46:56 +0200

    # reversed tests

--- start_BIDS_ConverteR.R
+++ start_BIDS_ConverteR.R
@@ -2,28 +2,6 @@
 args = commandArgs(trailingOnly=TRUE)
 
 
-# tests
-# test if there is at least one argument: if not, return an error
-if (length(args)==0) {
-  stop("At least one argument must be supplied (input directory of your study, containing 'dicom/session/subject' folders", call.=FALSE)
-} 
-
-# test if the path exists
-if(!dir.exists(args)) {
-  stop("Your path does not exist. Please choose a valid one.")
-}
-
-# test if the path contains a folder named "dicom"
-if(!dir.exists(paste0(args, "/dicom"))){
-    stop("Your path does not contain the 'dicom' root folder.")
-}
-
-dicomdirs = dir(path = args, pattern = "DICOM$|DICOMDIR$", recursive = TRUE)
-
-# test if the path contains subfolders named "DICOM" or "DICOMDIR"
-if(length(dicomdirs) == 0){
-  stop("No folder 'DICOM' or 'DICOMDIR' found in your subject folders.")
-}
 
 
 # print the number of all dicom folders
